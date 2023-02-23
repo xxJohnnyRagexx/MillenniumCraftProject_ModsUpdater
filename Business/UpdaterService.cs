c636d65e14a138d85c12656af2058528e62f536d
Sergey Tihomirov <ragej16@gmail.com>
2023-02-24 00:44:07 +0400

    Добавил даунлойдер.

--- Business/UpdaterService.cs
+++ Business/UpdaterService.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Dto;
+using Downloader;
 using FakeDAL;
 using System;
 using System.Collections.Generic;
@@ -12,11 +13,18 @@ namespace Business
     public class UpdaterService
     {
         private readonly IUpdatesClient _updatesClient;
+        private readonly IDownloadService _downloadService;
         public UpdaterService(IUpdatesClient updatesClient) { }
 
         public UpdaterService()
         {
+            var downloadOptions = new DownloadConfiguration
+            {
+                MaximumBytesPerSecond = 1024 * 1024 * 5,
+                ReserveStorageSpaceBeforeStartingDownload = true,
+            };
             _updatesClient = new UpdatesClient();
+            _downloadService = new DownloadService(downloadOptions);
         }
 
         public List<UpdateItemDto> GetUpdates()
@@ -26,6 +34,17 @@ namespace Business
                ).ToList();
 
             return q;
+        }
+
+        public async Task GetUpdateAsync(string version, EventHandler<DownloadProgressChangedEventArgs> downloadProcessChanged)
+        {
+            string file = @"D:\DownLoadedFile.zip";
+            //string url = $"http://localhost:5000/update/?gameVersion={version}";
+            string url = @"https://dl.zaycev.net/track/24922020/3gyDk3fqEpGD676kKB5rkkSkn4dK5aKCWEvjqHzytAuQka6hmwUGbugmH6CgGqHnyQWv3X1knCDrd2r5HboUKrhmn1uLUGkY9VMhshVuAx12QYQoWvNsa8TmkJcXykrYoksd2JTqhgN6RbWyuR9TRUHXn7SFvkXKAbv9CNw6BfyT4DkBoYuFZAo97UVceNu3p7WubbfmHGpY8pYmK4ttZJt9GQNSiHyigDFD2STcDYZbn8ReMiriBWoLqSFXxz3X7KTDMmiMVSvN8royZJiU6kWPvEy1aStTjJYprvkBZUF8waFGaoB9m9QFDuMsfDnzfqoLNtyGcm2MXErTp9f3WHTNGA26iaWB4pY5QdfE5MsnTnRjbYzJRWxRHSXGKSeaPnGUWjA7iy";
+            _downloadService.DownloadProgressChanged += downloadProcessChanged;
+            await _downloadService.DownloadFileTaskAsync(url, file);
+            
+            //BaseUrl/update/{version}
         }
     }
 }
--- ModsUpdater/ViewModels/VersionsListViewModel.cs
+++ ModsUpdater/ViewModels/VersionsListViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Avalonia.Controls.Selection;
+using Avalonia.Controls.Templates;
 using Business;
 using CommunityToolkit.Mvvm.Input;
+using Downloader;
 using System;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -12,9 +14,17 @@ namespace ModsUpdater.ViewModels
     {
         public ObservableCollection<VersionsViewModel> VersionsList { get; set; }
 
-        public string ShowedDescription { get; set; }
+        public ICommand DownloadFileCommand { get; }
 
         private VersionsViewModel selectedItem;
+
+        private int progress;
+
+        public int Progress
+        {
+            get => progress;
+            set => this.SetProperty(ref progress, value);
+        }
 
         public VersionsViewModel SelectedItem
         {
@@ -23,6 +33,7 @@ namespace ModsUpdater.ViewModels
         }
 
         private readonly UpdaterService _updaterService = new UpdaterService();
+        private readonly DownloadService _downloadService = new DownloadService();
         public VersionsListViewModel() 
         {
             var data = _updaterService.GetUpdates();
@@ -32,6 +43,18 @@ namespace ModsUpdater.ViewModels
                     x.ToModel()
                 )
             ));
+            DownloadFileCommand = new AsyncRelayCommand(async () =>
+            {
+                Progress = 0;
+               // var url = @"https://dl.zaycev.net/track/24922020/3gyDk3fqEpGD676kKB5rkkSkn4dK5aKCWEvjqHzytAuQka6hmwUGbugmH6CgGqHnyQWv3X1knCDrd2r5HboUKrhmn1uLUGkY9VMhshVuAx12QYQoWvNsa8TmkJcXykrYoksd2JTqhgN6RbWyuR9TRUHXn7SFvkXKAbv9CNw6BfyT4DkBoYuFZAo97UVceNu3p7WubbfmHGpY8pYmK4ttZJt9GQNSiHyigDFD2STcDYZbn8ReMiriBWoLqSFXxz3X7KTDMmiMVSvN8royZJiU6kWPvEy1aStTjJYprvkBZUF8waFGaoB9m9QFDuMsfDnzfqoLNtyGcm2MXErTp9f3WHTNGA26iaWB4pY5QdfE5MsnTnRjbYzJRWxRHSXGKSeaPnGUWjA7iy";
+                //var file = @"D:\Archive.zip";
+                //_downloadService.DownloadProgressChanged += (sender, e) => { Progress = Convert.ToInt32(e.ProgressPercentage * 100); };
+               // await _downloadService.DownloadFileTaskAsync(url, file);
+                await _updaterService.GetUpdateAsync(selectedItem.GameVersion,  (sender, e) => {
+                    Progress = Convert.ToInt32(e.ProgressPercentage * 100);
+                }
+                );
+            });
         }
 
     }
