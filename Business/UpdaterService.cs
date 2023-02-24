8a0dbbc7818cd437ab6c352b930a6512d9d2d91d
Sergey Tihomirov <ragej16@gmail.com>
2023-02-24 14:26:39 +0400

    Добавлена LiteDB

--- Business/UpdaterService.cs
+++ Business/UpdaterService.cs
@@ -1,11 +1,6 @@
 ﻿using Business.Dto;
+using DAL;
 using Downloader;
-using FakeDAL;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UpdatesServiceHttpClient;
 
 namespace Business
@@ -14,6 +9,7 @@ namespace Business
     {
         private readonly IUpdatesClient _updatesClient;
         private readonly IDownloadService _downloadService;
+        private readonly UpdatesInfoRepository _repository;
         public UpdaterService(IUpdatesClient updatesClient) { }
 
         public UpdaterService()
@@ -27,12 +23,22 @@ namespace Business
             _downloadService = new DownloadService(downloadOptions);
         }
 
-        public List<UpdateItemDto> GetUpdates()
+        public async Task<List<UpdateItemDto>> GetUpdates()
         {
             var q  = _updatesClient.FetchUpdates().Select(
                 x => x.ToDto()
                ).ToList();
 
+            var updatesInfo = await _repository.GetUpdatesInfoAsync();
+            var items = updatesInfo.Select(x => x.ToDto());
+            foreach (var item in items)
+            {
+                q.Remove(
+                    q.Where(x => x.Version == item.Version 
+                    && x.GameVersion == item.GameVersion)
+                    .First()
+                    );
+            }
             return q;
         }
 
@@ -40,11 +46,9 @@ namespace Business
         {
             string file = @"D:\DownLoadedFile.zip";
             string url = $"http://localhost:5000/api/updates-service/update?gameVersion={version}";
-            //string url = @"https://dl.zaycev.net/track/24922020/3gyDk3fqEpGD676kKB5rkkSkn4dK5aKCWEvjqHzytAuQka6hmwUGbugmH6CgGqHnyQWv3X1knCDrd2r5HboUKrhmn1uLUGkY9VMhshVuAx12QYQoWvNsa8TmkJcXykrYoksd2JTqhgN6RbWyuR9TRUHXn7SFvkXKAbv9CNw6BfyT4DkBoYuFZAo97UVceNu3p7WubbfmHGpY8pYmK4ttZJt9GQNSiHyigDFD2STcDYZbn8ReMiriBWoLqSFXxz3X7KTDMmiMVSvN8royZJiU6kWPvEy1aStTjJYprvkBZUF8waFGaoB9m9QFDuMsfDnzfqoLNtyGcm2MXErTp9f3WHTNGA26iaWB4pY5QdfE5MsnTnRjbYzJRWxRHSXGKSeaPnGUWjA7iy";
             _downloadService.DownloadProgressChanged += downloadProcessChanged;
             await _downloadService.DownloadFileTaskAsync(url, file);
-            
-            //BaseUrl/update/{version}
+
         }
     }
 }
